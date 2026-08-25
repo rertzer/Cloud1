742f3dbfc31634acfd20a31078a51ff12e9788b9
rertzer <rertzer@student.42.fr>
2026-08-25 13:59:18 +0200

    vagrant: role install_docker OK

--- vagrant-target/vagrant-home/tools/pswd_generator.sh
+++ vagrant-target/vagrant-home/tools/pswd_generator.sh
@@ -5,11 +5,11 @@ role_path="/home/vagrant/playdoh/roles/install_inception"
 sql_pass=`head /dev/random | basenc --base64url | head -c 32`
 sql_root_pass=`head /dev/random | basenc --base64url | head -c 32`
 wp_pass=`head /dev/random | basenc --base64url| head -c 32`
-vault_pass=`head /dev/random | basenc --base64url| head -c 64` > ${role_path}/files/vault.key
+head /dev/random | basenc --base64url| head -c 64 > ${role_path}/files/vault.key
 
 
 sed -i "s/^sql_password:.*$/sql_password: ${sql_pass}/" ${role_path}/vars/vagrant.yml
 sed -i "s/^sql_root_password:.*$/sql_root_password: ${sql_root_pass}/" ${role_path}/vars/vagrant.yml
 
-ansible_vault encrypt ${var_path}/vagrant.yml ${role_path}/files/vault.key
+ ansible-vault encrypt ${role_path}/vars/vagrant.yml --vault-password-file ${role_path}/files/vault.key --skip-tty-check
 
