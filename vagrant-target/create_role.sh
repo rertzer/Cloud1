55828fc89db45ba9eebc8aa3133d6a068b1c7b30
rertzer <rertzer@student.42.fr>
2026-08-25 11:13:15 +0200

    vagrant: reorganized in roles

--- vagrant-target/create_role.sh
+++ vagrant-target/create_role.sh
@@ -6,10 +6,12 @@ then
 	return 1
 fi
 
-mkdir -p roles/$1/files
-mkdir  roles/$1/templates
-mkdir  roles/$1/tasks
-mkdir  roles/$1/handlers
-mkdir  roles/$1/defaults
-mkdir  roles/$1/meta
-mkdir  roles/$1/vars
+roles_path="playdoh"
+
+mkdir  -p ${roles_path}/roles/$1/files
+mkdir  ${roles_path}/roles/$1/templates
+mkdir  ${roles_path}/roles/$1/tasks
+mkdir  ${roles_path}/roles/$1/handlers
+mkdir  ${roles_path}/roles/$1/defaults
+mkdir  ${roles_path}/roles/$1/meta
+mkdir  ${roles_path}/roles/$1/vars
