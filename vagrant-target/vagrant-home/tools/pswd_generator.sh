72c936c5466cb791f24905a951ec690563221c5a
rertzer <rertzer@student.42.fr>
2026-08-26 15:14:14 +0200

    vagrant: it works

--- vagrant-target/vagrant-home/tools/pswd_generator.sh
+++ vagrant-target/vagrant-home/tools/pswd_generator.sh
@@ -8,8 +8,10 @@ wp_pass=`head /dev/random | basenc --base64url| head -c 32`
 head /dev/random | basenc --base64url| head -c 64 > ${role_path}/files/vault.key
 
 
-sed -i "s/^sql_password:.*$/sql_password: ${sql_pass}/" ${role_path}/vars/vagrant.yml
-sed -i "s/^sql_root_password:.*$/sql_root_password: ${sql_root_pass}/" ${role_path}/vars/vagrant.yml
+sed -i "s/^sql_password:.*$/sql_password: ${sql_pass}/" ${role_path}/vars/main.yml
+sed -i "s/^sql_root_password:.*$/sql_root_password: ${sql_root_pass}/" ${role_path}/vars/main.yml
 
- ansible-vault encrypt ${role_path}/vars/vagrant.yml --vault-password-file ${role_path}/files/vault.key --skip-tty-check
+ ansible-vault encrypt ${role_path}/vars/main.yml --vault-password-file ${role_path}/files/vault.key
+ chown vagrant:vagrant ${role_path}/vars/main.yml
+ chown vagrant:vagrant ${role_path}/files/vault.key
 
